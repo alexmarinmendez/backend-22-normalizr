6586fefc26dc30faa0cc442855f9420c627131a2
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-10-27 20:41:07 -0500

    feat: add desafios

---
+++ desafios/1/resolucion/index.js
@@ -0,0 +1,28 @@
+const { normalize, schema } = require("normalizr");
+const fs = require('fs')
+
+const empresa = JSON.parse(fs.readFileSync('./empresa.json'))
+
+// Definimos un esquema de empleado
+const empleadoEntity = new schema.Entity('empleados');
+
+// Definimos un esquema de organigrama
+const empresaEntity = new schema.Entity('empresas', {
+  gerente: empleadoEntity,
+  encargado: empleadoEntity,
+  empleados: [empleadoEntity]
+});
+
+/* ---------------------------------------------------------------------------------------- */
+const util = require('util')
+
+function print(objeto) {
+  console.log(util.inspect(objeto, false, 12, true))
+}
+
+console.log(' ------------- OBJETO NORMALIZADO --------------- ')
+const normalizedEmpresa = normalize(empresa, empresaEntity);
+print(normalizedEmpresa)
+
+console.log('Longitud objeto original: ', JSON.stringify(empresa).length)
+console.log('Longitud objeto normalizado: ', JSON.stringify(normalizedEmpresa).length)
